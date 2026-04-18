53100a1b064d4185b05dcba355ec233aaedfea60
h38ist <okcan723@gmail.com>
2026-04-18 22:59:07 +0300

    Create getProjectMeta.js

---
+++ src/utils/getProjectMeta.js
@@ -0,0 +1,9 @@
+function getProjectMeta() {
+  return {
+    name: "TurkeyWeather API",
+    version: "1.0.0",
+    description: "Turkey cities weather api project"
+  };
+}
+
+module.exports = getProjectMeta;
