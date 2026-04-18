9b38edd08e8ede446adfda2de265ae6e585f26f7
h38ist <okcan723@gmail.com>
2026-04-18 22:59:47 +0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const getApiOverview = require("../utils/getApiOverview");
+const getProjectMeta = require("../utils/getProjectMeta");
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.json({
     success: true,
+    project: getProjectMeta(),
     message: "Welcome to TurkeyWeather API",
     requestNumber: req.requestNumber,
     endpoints: getApiOverview()
