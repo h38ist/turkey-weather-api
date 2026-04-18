bcc61bd56e46912a93df5b350e905754ba4a8a25
h38ist <okcan723@gmail.com>
2026-04-18 21:49:11 +0300

    Create weatherService.js

---
+++ src/services/weatherService.js
@@ -0,0 +1,20 @@
+const axios = require("axios");
+
+async function getCurrentWeatherByCoords(lat, lon) {
+  const apiKey = process.env.OPENWEATHER_API_KEY;
+
+  const response = await axios.get("https://api.openweathermap.org/data/2.5/weather", {
+    params: {
+      lat,
+      lon,
+      appid: apiKey,
+      units: "metric"
+    }
+  });
+
+  return response.data;
+}
+
+module.exports = {
+  getCurrentWeatherByCoords
+};
