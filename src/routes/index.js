29722ad3abb5767a678dee6b55387811d5925be7
h38ist <okcan723@gmail.com>
2026-04-18 22:37:36 +0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,11 +6,14 @@ router.get("/", (req, res) => {
   res.json({
     success: true,
     message: "Welcome to TurkeyWeather API",
+    requestNumber: req.requestNumber,
     endpoints: [
       "/health",
       "/api",
       "/api/cities",
       "/api/cities?region=ege",
+      "/api/cities?search=ist",
+      "/api/cities?limit=2",
       "/api/weather?city=Istanbul",
       "/api/weather/forecast?city=Ankara"
     ]
