2751ee7725fbd5dd489cdf6c75d177dbe2d8f8c4
h38ist <okcan723@gmail.com>
2026-04-18 21:53:08 +0300

    Update weatherService.js

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -15,6 +15,22 @@ async function getCurrentWeatherByCoords(lat, lon) {
   return response.data;
 }
 
+async function getForecastByCoords(lat, lon) {
+  const apiKey = process.env.OPENWEATHER_API_KEY;
+
+  const response = await axios.get("https://api.openweathermap.org/data/2.5/forecast", {
+    params: {
+      lat,
+      lon,
+      appid: apiKey,
+      units: "metric"
+    }
+  });
+
+  return response.data;
+}
+
 module.exports = {
-  getCurrentWeatherByCoords
+  getCurrentWeatherByCoords,
+  getForecastByCoords
 };
